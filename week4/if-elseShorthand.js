4e3abb0dfcb81fdb0d1ae02fe17f8f8b6c9a8cc2
WarisaTT <warisa.thia@mail.kmutt.ac.th>
2023-09-12 00:23:41 +0700

    seven commit

--- week4/if-elseShorthand.js
+++ week4/if-elseShorthand.js
@@ -5,15 +5,19 @@ let discount = 0
 let x = 5 //5
 console.log(x)
 if (someone) {
-  let x = 1
-  con
+  let x = 1 //1
   who = 'member'
-  discount = 0.1
+  discount = 0.10
 } else {
+  let x = 2//noerror 2
   who = 'guest'
   discount = 0.05
 }
-//?:
-someone ? (who = 'member') : (who = 'guest') //ปกติอันนี้ไม่ได้แต่ได้เฉย
-who = someone ? 'member' : 'guest'
+console.log(x)
 console.log(who)
+console.log(discount)
+
+//?:
+// someone ? (who = 'member') : (who = 'guest') //ปกติอันนี้ไม่ได้แต่ได้เฉย
+who = someone ? 'member' : 'guest'
+console.log(who) //guest
